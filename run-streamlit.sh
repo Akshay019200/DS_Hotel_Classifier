e259ffb6eb28950d8b552b655bce6b371b4729ef
Akshay019200 <uakshaykumarj@gmail.com>
2024-09-14 14:53:39 +0530

    run-streamlit.sh

--- run-streamlit.sh
+++ run-streamlit.sh
@@ -2,4 +2,4 @@
 
 source /home/adminuser/venv/bin/activate
 export STREAMLIT_HEALTH_CHECK_TIMEOUT=300
-streamlit run --server.maxMessageSize 100 Deployment.py
+streamlit run --verbose --server.maxMessageSize 100 Deployment.py
